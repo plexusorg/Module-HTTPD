e0fbe479c587622a8514614dbe74602cab6bff3d
Taah <spacerocket62@gmail.com>
2022-04-02 13:14:10 -0700

    uh idk what i changed

--- src/main/java/dev/plex/request/impl/GetEndpoints.java
+++ src/main/java/dev/plex/request/impl/GetEndpoints.java
@@ -11,11 +11,11 @@ import dev.plex.request.AbstractServlet;
 import dev.plex.request.GetMapping;
 import dev.plex.util.PlexLog;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.UUID;
-import java.util.stream.Collectors;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 //@RestController
 //@RequestMapping("/api/admins")
@@ -46,12 +46,12 @@ public class GetEndpoints extends AbstractServlet
         {
             PlexLog.debug("Plex-HTTPD using permissions check");
             final OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(UUID.fromString(player.getUuid()));
-            if (!HTTPDModule.getPermissions().playerHas(null, offlinePlayer, "plex.httpd.indefbans.access"))
+            if (!HTTPDModule.getPermissions().playerHas(null, offlinePlayer, "plex.httpd.admins.access"))
             {
                 return new GsonBuilder().setPrettyPrinting().create().toJson(Plex.get().getAdminList().getAllAdminPlayers().stream().peek(plexPlayer -> plexPlayer.setIps(Lists.newArrayList())).collect(Collectors.toList()));
             }
         }
-        return new GsonBuilder().setPrettyPrinting().create().toJson(new ArrayList<>(Plex.get().getAdminList().getAllAdminPlayers()));
+        return new GsonBuilder().setPrettyPrinting().create().toJson(Plex.get().getAdminList().getAllAdminPlayers());
     }
 
     @GetMapping(endpoint = "/api/indefbans/")
@@ -84,6 +84,6 @@ public class GetEndpoints extends AbstractServlet
                 return "Not enough permissions to view this page.";
             }
         }
-        return new GsonBuilder().setPrettyPrinting().create().toJson(new ArrayList<>(Plex.get().getPunishmentManager().getIndefiniteBans().stream().toList()));
+        return new GsonBuilder().setPrettyPrinting().create().toJson(Plex.get().getPunishmentManager().getIndefiniteBans().stream().toList());
     }
 }

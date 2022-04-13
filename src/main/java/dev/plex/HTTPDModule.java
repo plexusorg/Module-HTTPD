04c2f0e97d9a9b934c7c4df60260178387819faf
Telesphoreo <me@telesphoreo.me>
2022-04-12 20:15:09 -0500

    Update HTTPDModule.java

--- src/main/java/dev/plex/HTTPDModule.java
+++ src/main/java/dev/plex/HTTPDModule.java
@@ -7,6 +7,8 @@ import dev.plex.request.impl.IndefBansEndpoint;
 import dev.plex.request.impl.ListEndpoint;
 import dev.plex.request.impl.PunishmentsEndpoint;
 import dev.plex.util.PlexLog;
+import java.io.File;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 import lombok.Getter;
 import net.milkbowl.vault.permission.Permission;
@@ -35,7 +37,6 @@ public class HTTPDModule extends PlexModule
     @Override
     public void load()
     {
-        //moduleConfig = new ModuleConfig(this, this.getClass().getClassLoader().getResource("settings.yml").getFile());
         moduleConfig = new ModuleConfig(this, "settings.yml");
     }
 

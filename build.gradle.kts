32f17a23f3c6c901257adb7fe99d4e38bc2e173b
Telesphoreo <me@telesphoreo.me>
2022-05-08 13:59:50 -0500

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.projectlombok:lombok:1.18.22")
-    annotationProcessor("org.projectlombok:lombok:1.18.22")
+    implementation("org.projectlombok:lombok:1.18.24")
+    annotationProcessor("org.projectlombok:lombok:1.18.24")
     implementation("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
     implementation("dev.plex:server:1.1-SNAPSHOT")
     implementation("dev.plex:api:1.1-SNAPSHOT")
@@ -38,31 +38,8 @@ dependencies {
     implementation("org.eclipse.jetty:jetty-server:11.0.9")
     implementation("org.eclipse.jetty:jetty-servlet:11.0.9")
     implementation("org.eclipse.jetty:jetty-proxy:11.0.9")
-    implementation("com.github.MilkBowl:VaultAPI:1.7")
-}
-
-tasks {
-    compileJava {
-        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
-
-        // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
-        // See https://openjdk.java.net/jeps/247 for more information.
-        options.release.set(17)
-    }
-    javadoc {
-        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
-    }
-    processResources {
-        filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
-    }
-}
-
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-        }
+    implementation("com.github.MilkBowl:VaultAPI:1.7") {
+        exclude("org.bukkit", "bukkit")
     }
 }
 
@@ -72,5 +49,29 @@ tasks.getByName<Jar>("jar") {
     archiveVersion.set("")
     from("src/main/resources") {
         exclude("dev/**")
+    }
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+tasks {
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
     }
 }

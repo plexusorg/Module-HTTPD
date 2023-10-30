4e593e1b5ebd70e1e6ea7bfa644f0a7432718f21
Telesphoreo <me@telesphoreo.me>
2023-10-29 22:59:33 -0500

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,15 +28,15 @@ repositories {
 }
 
 dependencies {
-    implementation("org.projectlombok:lombok:1.18.28")
-    annotationProcessor("org.projectlombok:lombok:1.18.28")
-    implementation("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+    implementation("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    implementation("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
     implementation("dev.plex:server:1.4-SNAPSHOT")
-    implementation("org.json:json:20230618")
+    implementation("org.json:json:20231013")
     implementation("org.reflections:reflections:0.10.2")
-    implementation("org.eclipse.jetty:jetty-server:11.0.15")
-    implementation("org.eclipse.jetty:jetty-servlet:11.0.15")
-    implementation("org.eclipse.jetty:jetty-proxy:11.0.15")
+    implementation("org.eclipse.jetty:jetty-server:11.0.17")
+    implementation("org.eclipse.jetty:jetty-servlet:11.0.17")
+    implementation("org.eclipse.jetty:jetty-proxy:11.0.17")
     implementation("com.github.MilkBowl:VaultAPI:1.7.1") {
         exclude("org.bukkit", "bukkit")
     }

44dc3c1e9e4ebfdcd44c5b5934007a13384590b6
Telesphoreo <me@telesphoreo.me>
2024-10-16 16:33:52 -0500

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "dev.plex"
-version = "1.4-SNAPSHOT"
+version = "1.5-SNAPSHOT"
 description = "Module-HTTPD"
 
 repositories {
@@ -25,24 +25,26 @@ repositories {
             includeGroup("com.github.MilkBowl")
         }
     }
+
+    maven { url = uri("https://maven.enginehub.org/repo/") }
 }
 
 dependencies {
-    implementation("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
-    implementation("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
-    implementation("dev.plex:server:1.4-SNAPSHOT")
-    implementation("org.json:json:20231013")
+    implementation("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+    implementation("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+    implementation("dev.plex:server:1.5-SNAPSHOT")
+    implementation("org.json:json:20240303")
     implementation("org.reflections:reflections:0.10.2")
-    implementation("org.eclipse.jetty:jetty-server:11.0.19")
-    implementation("org.eclipse.jetty:jetty-servlet:11.0.19")
-    implementation("org.eclipse.jetty:jetty-proxy:11.0.19")
+    implementation("org.eclipse.jetty:jetty-server:11.0.24")
+    implementation("org.eclipse.jetty:jetty-servlet:11.0.24")
+    implementation("org.eclipse.jetty:jetty-proxy:11.0.24")
     implementation("com.github.MilkBowl:VaultAPI:1.7.1") {
         exclude("org.bukkit", "bukkit")
     }
-    implementation(platform("com.intellectualsites.bom:bom-newest:1.40")) // Ref: https://github.com/IntellectualSites/bom
-    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Core")
-    implementation("commons-io:commons-io:2.15.1")
+    implementation(platform("com.intellectualsites.bom:bom-newest:1.49")) // Ref: https://github.com/IntellectualSites/bom
+    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Core:2.11.2")
+    implementation("commons-io:commons-io:2.17.0")
 }
 
 tasks.getByName<Jar>("jar") {
@@ -55,7 +57,7 @@ tasks.getByName<Jar>("jar") {
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 tasks {
